6899f37989ad3b07da085ca5913b824a79ed535f
sau22rane <scrane22301@gmail.com>
2020-10-13 04:15:09 +0530

    Arrays added

--- DAOA/Pracs/1_General/Arrays/KthRank.java
+++ DAOA/Pracs/1_General/Arrays/KthRank.java
@@ -24,77 +24,76 @@ public class KthRank{
     } 
 }
 
-class MaxHeap { 
-    private int[] Heap; 
-    private int size; 
-    private int maxsize; 
-  
-    public MaxHeap(int maxsize) 
-    { 
-        this.maxsize = maxsize; 
-        this.size = 0; 
-        Heap = new int[this.maxsize + 1]; 
-        Heap[0] = Integer.MAX_VALUE; 
-    } 
-  
+class MaxHeap {
+    private int[] Heap;
+    private int size;
+    private int maxsize;
+
+    public MaxHeap(int maxsize){
+        this.maxsize = maxsize;
+        this.size = 0;
+        Heap = new int[this.maxsize + 1];
+        Heap[0] = Integer.MAX_VALUE;
+    }
+
     private int parent(int pos){return pos/2;}
   
     
-    private int leftChild(int pos){return (2 * pos);} 
-    private int rightChild(int pos){return (2 * pos) + 1;} 
+    private int leftChild(int pos){return (2 * pos);}
+    private int rightChild(int pos){return (2 * pos) + 1;}
   
-    private boolean isLeaf(int pos){ 
-        if (pos >= (size / 2) && pos <= size){ 
-            return true; 
+    private boolean isLeaf(int pos){
+        if (pos >= (size / 2) && pos <= size){
+            return true;
         }
-        return false; 
+        return false;
     }
-  
-    private void swap(int fpos, int spos){ 
-        int tmp; 
-        tmp = Heap[fpos]; 
-        Heap[fpos] = Heap[spos]; 
-        Heap[spos] = tmp; 
-    } 
-  
 
-    private void maxHeapify(int pos) 
-    { 
-        if (isLeaf(pos)) 
-            return; 
-  
-        if (Heap[pos] < Heap[leftChild(pos)] ||  
-            Heap[pos] < Heap[rightChild(pos)]) { 
-  
-            if (Heap[leftChild(pos)] > Heap[rightChild(pos)]) { 
-                swap(pos, leftChild(pos)); 
-                maxHeapify(leftChild(pos)); 
-            } 
-            else { 
-                swap(pos, rightChild(pos)); 
-                maxHeapify(rightChild(pos)); 
-            } 
-        } 
-    } 
-  
-    public void insert(int element) 
-    { 
-        Heap[++size] = element; 
-  
-        int current = size; 
-        while (Heap[current] > Heap[parent(current)]) { 
-            swap(current, parent(current)); 
-            current = parent(current); 
-        } 
-    } 
-  
+    private void swap(int fpos, int spos){
+        int tmp;
+        tmp = Heap[fpos];
+        Heap[fpos] = Heap[spos];
+        Heap[spos] = tmp;
+    }
+
+
+    private void maxHeapify(int pos)
+    {
+        if (isLeaf(pos))
+            return;
+
+        if (Heap[pos] < Heap[leftChild(pos)] || Heap[pos] < Heap[rightChild(pos)]){
+
+            if (Heap[leftChild(pos)] > Heap[rightChild(pos)]){
+                swap(pos, leftChild(pos));
+                maxHeapify(leftChild(pos));
+            }
+            else{
+                swap(pos, rightChild(pos));
+                maxHeapify(rightChild(pos));
+            }
+        }
+    }
+
+    public void insert(int element)
+    {
+        size++;
+        Heap[size] = element;
+
+        int current = size;
+        while(Heap[current] > Heap[parent(current)]){
+            swap(current, parent(current));
+            current = parent(current);
+        }
+    }
+
     
     
-    public int popMax() 
-    { 
-        int popped = Heap[1]; 
-        Heap[1] = Heap[size--]; 
-        maxHeapify(1); 
+    public int popMax()
+    {
+        int popped = Heap[1];
+        Heap[1] = Heap[size--];
+        maxHeapify(1);
         return popped; 
-    } 
-} 
+    }
+}
