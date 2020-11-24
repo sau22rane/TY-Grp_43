54a9e527ed38166083635aa18f2a759fc6e4792b
tarun42 <dastarun42@gmail.com>
2020-11-24 18:01:06 +0530

    ASE project

---
+++ ASE/CourseProject/YogaApp/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:YogaApp/ListOfAsanas.dart';
+import 'package:YogaApp/courseList.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final dbref = FirebaseDatabase.instance.reference();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Yoga",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("YOGA-APP"),
+        ),
+        body: Center(child: CourseList()),
+        // body: ListOfAsanas(dbref, "stress"),
+      ),
+    );
+  }
+}
