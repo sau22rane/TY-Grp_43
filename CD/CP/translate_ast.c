ea6b6063142cedf5c54aa2c88126487cf6021d97
sau22rane <scrane22301@gmail.com>
2020-12-23 18:37:57 +0530

    Final Course Project Added

---
+++ CD/CP/translate_ast.c
@@ -0,0 +1,614 @@
+#include "c2java.h"
+
+
+typedef void (*ast_translator)(ast_node *n);
+
+static ast_translator g_ast_translators[AST_TYPE_LIMIT];
+static const char* g_ast_names[AST_TYPE_LIMIT];
+
+int dec_init = 0;
+
+void emit(const char *fmt, ...)
+{
+    va_list args;
+    char buffer[1024], format[1024];
+
+    va_start(args, fmt);
+    vsnprintf(buffer, sizeof buffer, fmt, args);
+    va_end(args);
+
+    sprintf(format, "%s", buffer);
+
+    fprintf(stdout, "%s", format);
+}
+
+char *split = "";
+int tabn = 0;
+void emit_tab() {
+	for (int i = 0; i < tabn; ++i) {
+		emit("\t");
+	}
+}
+
+static void trans_list(ast_node *n)
+{   static int count= 0;
+    count++;
+    int tcount = count;
+    // printf("\nStarting %dth list\n",tcount);
+    for (; n; n = n->list.tail)
+    {
+        trans_ast(n->list.head);
+        if (n->list.tail) emit("%s", split);
+    }
+    // printf("\nClosing %dth list\n",tcount);
+}
+
+static void trans_funcdef(ast_node *n)
+{
+	char *s;
+	if (n->funcdef.ret_type == 1) s = "int";
+	else if (n->funcdef.ret_type == 2) s = "float";
+	else if (n->funcdef.ret_type == 3) s = "char";
+	
+	emit_tab();
+    emit("static %s ", s);
+    trans_ast(n->funcdef.funchead);
+    emit(" {\n");
+	split = "";
+	++tabn;
+    trans_ast(n->funcdef.funcbody);
+    --tabn;
+    emit("\n");
+    emit_tab();
+    emit("}\n");
+}
+
+static void trans_funchead(ast_node *n)
+{	
+	char *s = symname(n->funchead.sym_name);
+	if (!strcmp(s, "main")) {
+		emit("main_(int argc, String[] args) ");
+		return;
+	}
+    emit("%s (", s);
+    split = ", ";
+    trans_ast(n->funchead.paras);
+	split = "";
+    emit(") ");
+}
+
+static void trans_para(ast_node *n)
+{
+	char *s = "";
+	if (n->para.para_type == 1) s = "int";
+	else if (n->para.para_type == 2) s = "float";
+	else if (n->para.para_type == 3) s = "char";
+    emit("%s ", s);
+    trans_ast(n->para.var);
+}
+
+int inclass = 0;
+
+static void trans_stdef(ast_node *n)
+{
+	emit_tab();
+	emit("static class %s {\n", symname(n->stdef.sym_name));
+	split = ";\n";
+	++tabn;
+	inclass = 1;
+    trans_ast(n->stdef.defs);
+    --tabn;
+    inclass = 0;
+	split = "";
+	emit_tab();
+    emit("};\n");
+}
+
+int inarray = 0;
+int array_depth = 0;
+int index1[100];
+int need_brac = 0;
+char *cur_type = "";
+char *cur_struct_varname = "";
+
+static void trans_def(ast_node *n)
+{	
+	char *s = "";
+	emit_tab();
+	if (n->fg == 'g') {
+		emit("static ");
+	}
+	if (inclass) emit("public ");
+	if (n->def.def_type == 1) {
+		s = "int";
+		cur_type = s;
+	}
+	else if (n->def.def_type == 2) {
+		s = "float";
+		cur_type = s;
+	}
+	else if (n->def.def_type == 3) {
+		s = "char";
+		cur_type = s;
+	}
+	else {
+		 s = "[]";
+	}
+    emit("%s ", s);
+	split = ", ";
+	trans_ast(n->def.decs);
+    split = "";
+	emit(";");
+    if (need_brac == 1) emit(" }");
+    need_brac = 0;
+    emit("\n");
+    
+}
+
+static void trans_dec(ast_node *n)
+{
+	array_depth = 0;
+	if (n->dec.init) dec_init = 1;
+	else dec_init = 0;
+    trans_ast(n->dec.var);
+    
+    if (n->dec.init)
+    {	
+    	emit(" = ");
+        ast_node *p = n->dec.init;
+        if (p->type == AST_LIST)
+        {
+            emit("{");
+        	trans_ast(n->dec.init);
+        	emit("}");
+        }
+        else
+        {
+        	trans_ast(n->dec.init);
+        }
+    }
+}
+
+static void trans_var(ast_node *n)
+{
+   emit("%s", symname(n->var.sym_name));
+}
+
+int cur_array_depth = 0;
+
+static void trans_subvar(ast_node *n)
+{
+	inarray = 1;
+	if (dec_init == 0) {
+		++cur_array_depth;
+		emit("[]");
+		if (n->subvar.var->type != AST_SUBVAR) {
+			emit(" ");
+			trans_ast(n->subvar.var);
+			emit(";\n");
+			emit_tab();
+			if (n->fg == 'g') emit("static { ");
+			trans_ast(n->subvar.var);
+			cur_struct_varname = symname(n->subvar.var->var.sym_name);
+			emit(" = new %s", cur_type);
+		}
+		else trans_ast(n->subvar.var);
+		emit("[");
+		emit("%d", n->subvar.index);
+		emit("]");
+		index1[cur_array_depth] = n->subvar.index;
+		if (!array_depth) array_depth = cur_array_depth;
+		--cur_array_depth;
+		if (n->fg == 'g' && cur_array_depth == 0) need_brac = 1;
+	}
+	else {
+		trans_ast(n->subvar.var);
+		emit("[]");
+	}
+}
+
+static void trans_compound_stmt(ast_node *n)
+{
+	split = "";
+	trans_ast(n->compound_stmt.defs);
+    trans_ast(n->compound_stmt.stmts);
+}
+
+static void trans_expr_stmt(ast_node *n)
+{	
+	emit_tab();
+    trans_ast(n->expr_stmt.expr);
+    emit(";\n");
+}
+
+static const char * binop_instr(int op)
+{
+    switch (op)
+    {
+    case ADD_ASSIGN:
+        return "+=";
+    case SUB_ASSIGN:
+        return "-=";
+    case MUL_ASSIGN:
+        return "*=";
+    case DIV_ASSIGN:
+        return "/=";
+    case MOD_ASSIGN:
+        return "%=";
+    case AND_ASSIGN:
+        return "&=";
+    case OR_ASSIGN:
+        return "|=";
+    case XOR_ASSIGN:
+        return "^=";
+    case SHL_ASSIGN:
+        return "<<=";
+    case SHR_ASSIGN:
+        return ">>=";
+    case GE_OP:
+        return ">=";
+    case LE_OP:
+        return "<=";
+    case EQ_OP:
+        return "==";
+    case NE_OP:
+        return "!=";
+    case SHL_OP:
+        return "<<";
+    case SHR_OP:
+        return ">>";
+    case AND_OP:
+		return "&&";
+	case OR_OP:
+		return "||";
+	case '+':
+        return "+";
+    case '-':
+        return "-";
+    case '*':
+        return "*";
+    case '/':
+        return "/";
+    case '%':
+        return "%";
+    case '^':
+        return "^";
+    case '&':
+        return "&";
+    case '|':
+        return "|";
+    case '>':
+        return ">";
+    case '<':
+        return "<";
+	case '=':
+		return "=";
+    }
+    return "Error??";
+}
+
+static int check_bool(int op) {
+	switch (op)
+	{
+	case GE_OP:
+        return 1;
+    case LE_OP:
+        return 1;  
+	case '>':
+        return 1;
+    case '<':
+        return 1;
+    case EQ_OP:
+        return 1;
+    case NE_OP:
+        return 1;
+	case AND_OP:
+		return 1;
+	case OR_OP:
+		return 1;
+	}
+	return 0;
+}
+
+int check_bool_lr_bool(int op) {
+	switch (op)
+	{
+	case AND_OP:
+		return 1;
+	case OR_OP:
+		return 1;
+	}
+	return 0;
+}
+
+
+static void trans_bool(ast_node *n, int mustbool) {
+	if (!mustbool) {
+		trans_ast(n);
+		return;
+	}
+	if (n->type == AST_PREFIX) {
+		if (n->prefix.op == '!') {
+			emit("(");
+			trans_bool(n->prefix.unary, 0);
+			emit(") == 0");
+			return;
+		}
+	}
+	if (n->type == AST_CONST) {
+		emit("%d != 0", n->constant.number);
+	}	
+	if (n->type == AST_ID) {
+		char *s = symname(n->id.sym_name);
+		emit("%s != 0", s);
+	}
+	if (n->type == AST_MEMBER) {
+		emit("(");
+		trans_ast(n->member.postfix);
+    	emit(".");
+    	emit(symname(n->member.sym_name));
+    	emit(") != 0");
+	}
+	if (n->type == AST_FUNC_CALL) {
+		emit("(");	
+		trans_ast(n->func_call.postfix);
+		emit("(");
+		if (n->func_call.args) {
+			split = ", ";
+			trans_ast(n->func_call.args);
+			split = "";
+		}
+		emit(")) != 0");
+	}
+	if (n->type == AST_INDEXING) {
+		emit("(");
+	    trans_ast(n->indexing.postfix);
+		emit("[");
+		trans_ast(n->indexing.expr);
+		emit("]");
+		emit(") != 0");
+	}
+	if (n->type == AST_POSTFIX) {
+		emit("(");
+	    trans_ast(n->postfix.unary);
+		if (n->prefix.op == INC_OP) emit("++");
+		else if (n->prefix.op == DEC_OP) emit("--");
+		emit(") != 0");	
+	}
+	if (n->type == AST_BINOP) {
+		int tobebool = check_bool_lr_bool(n->binop.op);
+		int alreadybool = check_bool(n->binop.op);
+		if (!alreadybool) emit("(");
+		trans_bool(n->binop.lhs, tobebool);
+		emit(" %s ", binop_instr(n->binop.op));
+		trans_bool(n->binop.rhs, tobebool);
+		if (!alreadybool) emit(") != 0");
+	}
+}
+
+
+static void trans_if_stmt(ast_node *n)
+{
+	emit_tab();
+	emit("if (");
+    trans_bool(n->if_stmt.cond, 1);
+    emit(") {\n");
+    ++tabn;
+    trans_ast(n->if_stmt.then);
+    --tabn;
+    emit_tab();
+    emit("}\n");
+    if (n->if_stmt.els) {
+    	emit_tab();
+    	emit("else {\n");
+    	++tabn;
+    	trans_ast(n->if_stmt.els);
+    	--tabn;
+    	emit_tab();
+    	emit("}\n");
+    }
+}
+
+static void trans_for_stmt(ast_node *n)
+{
+	emit_tab();
+	emit("for (");
+    trans_ast(n->for_stmt.init);
+    emit("; ");
+    trans_bool(n->for_stmt.cond, 1);
+    emit("; ");
+    trans_ast(n->for_stmt.incr);
+	emit(") {\n");
+	++tabn;
+    trans_ast(n->for_stmt.body);
+    --tabn;
+    emit_tab();
+    emit("}\n");
+}
+
+static void trans_return_stmt(ast_node *n)
+{
+	emit_tab();
+	emit("return ");
+    trans_ast(n->return_stmt.retval);
+    emit(";");
+}
+
+static void trans_continue_stmt(ast_node *n)
+{
+	emit_tab();
+    emit("continue;\n");
+}
+
+static void trans_break_stmt(ast_node *n)
+{
+	emit_tab();
+    emit("break;\n");
+}
+
+
+static void trans_binop(ast_node *n)
+{	
+    trans_ast(n->binop.lhs);
+    emit(" %s ", binop_instr(n->binop.op));
+    trans_ast(n->binop.rhs);
+}
+
+static void trans_prefix(ast_node *n)
+{
+    if (n->prefix.op == INC_OP) emit("++");
+    else if (n->prefix.op == DEC_OP) emit("--");
+	else emit("%c", n->prefix.op);
+    trans_ast(n->prefix.unary);
+}
+
+static void trans_postfix(ast_node *n)
+{	
+    trans_ast(n->postfix.unary);
+    if (n->prefix.op == INC_OP) emit("++");
+    else if (n->prefix.op == DEC_OP) emit("--");
+}
+
+static void trans_indexing(ast_node *n)
+{	
+    trans_ast(n->indexing.postfix);
+    emit("[");
+    trans_ast(n->indexing.expr);
+    emit("]");
+}
+
+static void trans_func_call(ast_node *n)
+{
+    trans_ast(n->func_call.postfix);
+    emit("(");
+    if (n->func_call.args) {
+    	split = ", ";
+    	trans_ast(n->func_call.args);
+    	split = "";
+    }
+    emit(")");
+}
+
+static void trans_member(ast_node *n)
+{
+    trans_ast(n->member.postfix);
+    emit(".");
+    emit(symname(n->member.sym_name));
+}
+
+static void trans_id(ast_node *n) {
+	char *s = symname(n->id.sym_name);
+	if (!strcmp(s, "write")) s = "System.out.println";
+	else if (!strcmp(s, "read")) s = "in.nextInt";
+	else if (!strcmp(s, "main")) s = "main_";
+	emit("%s", s);
+}
+
+static void trans_const(ast_node *n) {
+	emit("%d", n->constant.number);
+}
+
+int binop_reuse(int op)
+{
+    switch (op)
+    {
+    case '=':
+    case ADD_ASSIGN:
+    case SUB_ASSIGN:
+    case MUL_ASSIGN:
+    case DIV_ASSIGN:
+    case MOD_ASSIGN:
+    case AND_ASSIGN:
+    case OR_ASSIGN:
+    case XOR_ASSIGN:
+    case SHL_ASSIGN:
+    case SHR_ASSIGN:
+        return 1;
+    }
+    return 0;
+}
+
+
+static void init_ast_translators()
+{
+    if (g_ast_translators[0]) return;
+
+    g_ast_translators[AST_LIST] = trans_list;
+    g_ast_translators[AST_FUNCDEF] = trans_funcdef;
+    g_ast_translators[AST_FUNCHEAD] = trans_funchead;
+    g_ast_translators[AST_PARA] = trans_para;
+    g_ast_translators[AST_STDEF] = trans_stdef;
+    g_ast_translators[AST_VAR] = trans_var;
+    g_ast_translators[AST_SUBVAR] = trans_subvar;
+    g_ast_translators[AST_COMPOUND_STMT] = trans_compound_stmt;
+    g_ast_translators[AST_EXPR_STMT] = trans_expr_stmt;
+    g_ast_translators[AST_IF_STMT] = trans_if_stmt;
+    g_ast_translators[AST_FOR_STMT] = trans_for_stmt;
+    g_ast_translators[AST_RETURN_STMT] = trans_return_stmt;
+    g_ast_translators[AST_CONTINUE_STMT] = trans_continue_stmt;
+    g_ast_translators[AST_BREAK_STMT] = trans_break_stmt;
+    g_ast_translators[AST_DEF] = trans_def;
+    g_ast_translators[AST_DEC] = trans_dec;
+    g_ast_translators[AST_BINOP] = trans_binop;
+    g_ast_translators[AST_PREFIX] = trans_prefix;
+    g_ast_translators[AST_POSTFIX] = trans_postfix;
+    g_ast_translators[AST_INDEXING] = trans_indexing;
+    g_ast_translators[AST_FUNC_CALL] = trans_func_call;
+    g_ast_translators[AST_MEMBER] = trans_member;
+    g_ast_translators[AST_ID] = trans_id;
+    g_ast_translators[AST_CONST] = trans_const;
+
+    g_ast_names[AST_LIST] = "trans_list";
+    g_ast_names[AST_FUNCDEF] = "trans_funcdef";
+    g_ast_names[AST_FUNCHEAD] = "trans_funchead";
+    g_ast_names[AST_PARA] = "trans_para";
+    g_ast_names[AST_STDEF] = "trans_stdef";
+    g_ast_names[AST_VAR] = "trans_var";
+    g_ast_names[AST_SUBVAR] = "trans_subvar";
+    g_ast_names[AST_COMPOUND_STMT] = "trans_compound_stmt";
+    g_ast_names[AST_EXPR_STMT] = "trans_expr_stmt";
+    g_ast_names[AST_IF_STMT] = "trans_if_stmt";
+    g_ast_names[AST_FOR_STMT] = "trans_for_stmt";
+    g_ast_names[AST_RETURN_STMT] = "trans_return_stmt";
+    g_ast_names[AST_CONTINUE_STMT] = "trans_continue_stmt";
+    g_ast_names[AST_BREAK_STMT] = "trans_break_stmt";
+    g_ast_names[AST_DEF] = "trans_def";
+    g_ast_names[AST_DEC] = "trans_dec";
+    g_ast_names[AST_BINOP] = "trans_binop";
+    g_ast_names[AST_PREFIX] = "trans_prefix";
+    g_ast_names[AST_POSTFIX] = "trans_postfix";
+    g_ast_names[AST_INDEXING] = "trans_indexing";
+    g_ast_names[AST_FUNC_CALL] = "trans_func_call";
+    g_ast_names[AST_MEMBER] = "trans_member";
+    g_ast_names[AST_ID] = "trans_id";
+    g_ast_names[AST_CONST] = "trans_const";
+}
+
+void trans_ast(ast_node *n)
+{
+    init_ast_translators();
+    if (n)
+        g_ast_translators[n->type](n);
+}
+
+
+
+int main_sym;
+
+void transtext_ast(ast_node *n)
+{	
+	emit("import java.util.*;\n\n");
+	char *s = symname(3);
+	s = strtok(s, ".");
+    emit("public class %s\n{\n", s);
+    ++tabn;
+    emit_tab();
+    emit("static Scanner in = new Scanner(System.in);\n");   
+    
+    trans_ast(n);
+    
+    emit("\tpublic static void main(String[] args) {\n");
+    emit("\t\tSystem.exit(main_(args.length, args));\n\t}\n");
+    emit("}\n");
+}
+
---
+++ CD/CP/y.tab.h
@@ -0,0 +1,155 @@
+/* A Bison parser, made by GNU Bison 3.0.4.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    INC_OP = 258,
+    DEC_OP = 259,
+    SHL_OP = 260,
+    SHR_OP = 261,
+    LE_OP = 262,
+    GE_OP = 263,
+    EQ_OP = 264,
+    NE_OP = 265,
+    AND_OP = 266,
+    OR_OP = 267,
+    ADD_ASSIGN = 268,
+    SUB_ASSIGN = 269,
+    MUL_ASSIGN = 270,
+    DIV_ASSIGN = 271,
+    MOD_ASSIGN = 272,
+    AND_ASSIGN = 273,
+    XOR_ASSIGN = 274,
+    OR_ASSIGN = 275,
+    SHL_ASSIGN = 276,
+    SHR_ASSIGN = 277,
+    IDENTIFIER = 278,
+    CONSTANT = 279,
+    INT = 280,
+    STRUCT = 281,
+    FLOAT = 282,
+    CHAR = 283,
+    IF = 284,
+    ELSE = 285,
+    FOR = 286,
+    CONTINUE = 287,
+    BREAK = 288,
+    RETURN = 289,
+    READ = 290,
+    EXTDEFS = 291,
+    PARAS = 292,
+    STMTS = 293,
+    DEFS = 294,
+    DECS = 295,
+    ARGS = 296,
+    INIT_ARGS = 297
+  };
+#endif
+/* Tokens.  */
+#define INC_OP 258
+#define DEC_OP 259
+#define SHL_OP 260
+#define SHR_OP 261
+#define LE_OP 262
+#define GE_OP 263
+#define EQ_OP 264
+#define NE_OP 265
+#define AND_OP 266
+#define OR_OP 267
+#define ADD_ASSIGN 268
+#define SUB_ASSIGN 269
+#define MUL_ASSIGN 270
+#define DIV_ASSIGN 271
+#define MOD_ASSIGN 272
+#define AND_ASSIGN 273
+#define XOR_ASSIGN 274
+#define OR_ASSIGN 275
+#define SHL_ASSIGN 276
+#define SHR_ASSIGN 277
+#define IDENTIFIER 278
+#define CONSTANT 279
+#define INT 280
+#define STRUCT 281
+#define FLOAT 282
+#define CHAR 283
+#define IF 284
+#define ELSE 285
+#define FOR 286
+#define CONTINUE 287
+#define BREAK 288
+#define RETURN 289
+#define READ 290
+#define EXTDEFS 291
+#define PARAS 292
+#define STMTS 293
+#define DEFS 294
+#define DECS 295
+#define ARGS 296
+#define INIT_ARGS 297
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+
+union YYSTYPE
+{
+#line 5 "c2java.y" /* yacc.c:1909  */
+
+    int i;
+    struct ast_node *n;
+
+#line 143 "y.tab.h" /* yacc.c:1909  */
+};
+
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
